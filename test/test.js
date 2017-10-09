1cc0d571cbcd7a7076fd9e5786d5a3c63ed1a381
Robert Dickinson <bob.dickinson@gmail.com>
2017-10-08 22:32:47 -0700

    Completed unit tests

--- test/test.js
+++ test/test.js
@@ -11,7 +11,17 @@ var clientSecret = crypto.randomBytes(32).toString('hex');
 var _tokenInfo = 
 { 
     access_token: crypto.randomBytes(32).toString('hex'),
-    created_at: '2017-09-30T18:20:31.102Z',
+    created_at: new Date().toISOString(),
+    expires_in: 36000,
+    refresh_token: crypto.randomBytes(32).toString('hex'),
+    token_type: 'bearer',
+    account_id: 111111 
+}
+
+var _refreshTokenInfo = 
+{ 
+    access_token: crypto.randomBytes(32).toString('hex'),
+    created_at: new Date().toISOString(),
     expires_in: 36000,
     refresh_token: crypto.randomBytes(32).toString('hex'),
     token_type: 'bearer',
@@ -42,6 +52,22 @@ var errAuthFailure =
 
 var _users = []
 
+function createUser(userInfo)
+{
+    var user =
+    {
+        id: 10000000 + _users.length,
+        firstname: userInfo.first,
+        lastname: userInfo.last,
+        email: userInfo.email || (userInfo.first.toLowerCase() + '@pulp.com'),
+        username: userInfo.username || userInfo.first.toLowerCase(),
+        openid_name: userInfo.username || userInfo.first.toLowerCase(),
+        created_at: new Date().toISOString()
+    }
+
+    return user;
+}
+
 var userNames = 
 [
     { first: "Vincent", last: "Vega" },
@@ -54,18 +80,9 @@ var userNames =
     { first: "Antoine", last: "Roccamora" }
 ]
 
-userNames.forEach(function(username)
+userNames.forEach(function(userInfo)
 {
-    _users.push(
-    {
-        id: 10000000 + _users.length,
-        firstname: username.first,
-        lastname: username.last,
-        email: username.first.toLowerCase() + '@pulp.com',
-        username: username.first.toLowerCase(),
-        openid_name: username.first.toLowerCase(),
-        created_at: new Date().toISOString()
-    });
+    _users.push(createUser(userInfo));
 });
 
 var _apps = 
@@ -92,9 +109,9 @@ var _apps =
 
 describe('OneLogin', function() 
 {
-    describe('get token', function()
+    describe('token', function()
     {
-        it('fails with invalid credentials (live site)', function(done) 
+        it('getToken fails with invalid credentials (live site)', function(done) 
         {
             var oneLogin = new OneLogin("foo", "bar", "us");
             oneLogin.getToken(function(err, tokenInfo)
@@ -108,7 +125,7 @@ describe('OneLogin', function()
             });
         });
 
-        it('fails with invalid credentials', function(done) 
+        it('getToken fails with invalid credentials', function(done) 
         {
             nock('https://api.us.onelogin.com').post('/auth/oauth2/v2/token').reply(function(uri, requestBody) 
             {
@@ -125,12 +142,11 @@ describe('OneLogin', function()
                 assert.equal(err.code, errAuthFailure.status.code);
                 assert.equal(err.type, errAuthFailure.status.type);
                 assert.equal(err.message, errAuthFailure.status.message);
-                nock.cleanAll();
                 done();
             });
         });
 
-        it('succeeds', function(done) 
+        it('getToken succeeds', function(done) 
         {
             nock('https://api.us.onelogin.com').post('/auth/oauth2/v2/token').reply(function(uri, requestBody) 
             {
@@ -148,10 +164,65 @@ describe('OneLogin', function()
             {
                 assert.ifError(err);
                 assert.deepEqual(tokenInfo, _tokenInfo);
-                nock.cleanAll();
-                issuedToken = tokenInfo.access_token;
                 done();
             });
+        });
+
+        it('refreshToken succeeds', function(done) 
+        {
+            // Get token request
+            //
+            nock('https://api.us.onelogin.com').post('/auth/oauth2/v2/token').reply(function(uri, requestBody) 
+            {
+                assert(this.req.headers['authorization']);
+                assert.match(this.req.headers['authorization'], new RegExp('client_id:' + clientId + ', client_secret:' + clientSecret));
+
+                assert.equal(this.req.headers['content-type'], 'application/json');
+                assert.deepEqual(requestBody, { "grant_type": "client_credentials" });
+
+                return [ 200, _tokenInfo ];
+            });
+
+            // Refresh token request
+            //
+            nock('https://api.us.onelogin.com').post('/auth/oauth2/v2/token').reply(function(uri, requestBody) 
+            {
+                assert.equal(this.req.headers['content-type'], 'application/json');
+                assert.deepEqual(requestBody, 
+                { 
+                    grant_type: "refresh_token",
+                    access_token: _tokenInfo.access_token,
+                    refresh_token: _tokenInfo.refresh_token
+                });
+
+                return [ 200, _refreshTokenInfo ];
+            });
+
+            var oneLogin = new OneLogin(clientId, clientSecret, "us");
+            oneLogin.getToken(function(err, tokenInfo)
+            {
+                assert.ifError(err);
+                assert.deepEqual(tokenInfo, _tokenInfo);
+
+                var createdAt = _tokenInfo.created_at;
+                _tokenInfo.created_at = new Date(Date.parse(_tokenInfo.created_at) - (36001 * 1000)).toISOString();
+
+                oneLogin.refreshTokenIfNeeded(function(err, newTokenInfo)
+                {
+                    assert.ifError(err);
+                    assert.deepEqual(newTokenInfo, _refreshTokenInfo);
+                    _tokenInfo.created_at = createdAt;
+                    done();
+                })
+            });
+        });
+
+        afterEach(function()
+        {
+            // Nock should be clean after each test, but in case of a failure, we'll clean it after each
+            // test just to make sure it doesn't corrupt any subsequent tests.
+            //
+            nock.cleanAll();
         });
     });
 
@@ -219,19 +290,91 @@ describe('OneLogin', function()
             });
         });
 
-        it.skip('getUserApps', function(done)
+        it('getUserApps', function(done)
         {
+            nock('https://api.us.onelogin.com').get('/api/1/users/10000000/apps').reply(function(uri, requestBody) 
+            {
+                assert(this.req.headers['authorization']);
+                assert.match(this.req.headers['authorization'], new RegExp('bearer: ' + _tokenInfo.access_token));
 
+                var response = 
+                { 
+                    status: { error: false, code: 200, type: 'success', message: 'Success' },
+                    data: _apps
+                } 
+
+                return [ 200, response ];
+            });
+
+            var oneLogin = new OneLogin(clientId, clientSecret, "us");
+            oneLogin.getUserApps(10000000, function(err, apps)
+            {
+                assert.ifError(err);
+                assert.deepEqual(apps, _apps);
+                done();
+            });
         });
 
-        it.skip('createUser', function(done)
+        it('createUser', function(done)
         {
+            var userParams = 
+            {
+                username: "diehard",
+                firstname: "John",
+                lastname: "McClane",
+                email: "john@nakatomi.co.jp"
+            }
 
+            var createdUser;
+
+            nock('https://api.us.onelogin.com').post('/api/1/users').reply(function(uri, requestBody) 
+            {
+                assert(this.req.headers['authorization']);
+                assert.match(this.req.headers['authorization'], new RegExp('bearer: ' + _tokenInfo.access_token));
+
+                assert.deepEqual(requestBody, userParams);
+
+                var createdUser = createUser(userParams);
+
+                var response = 
+                { 
+                    status: { error: false, code: 200, type: 'success', message: 'Success' },
+                    data: createdUser
+                } 
+
+                return [ 200, response ];
+            });
+
+            var oneLogin = new OneLogin(clientId, clientSecret, "us");
+            oneLogin.createUser(userParams, function(err, user)
+            {
+                assert.ifError(err);
+                assert.deepEqual(user, createdUser);
+                done();
+            });
         });
 
-        it.skip('deleteUser', function(done)
+        it('deleteUser', function(done)
         {
+            nock('https://api.us.onelogin.com').delete('/api/1/users/10000000').reply(function(uri, requestBody) 
+            {
+                assert(this.req.headers['authorization']);
+                assert.match(this.req.headers['authorization'], new RegExp('bearer: ' + _tokenInfo.access_token));
 
+                var response = 
+                { 
+                    status: { error: false, code: 200, type: 'success', message: 'Success' }
+                } 
+
+                return [ 200, response ];
+            });
+
+            var oneLogin = new OneLogin(clientId, clientSecret, "us");
+            oneLogin.deleteUser(10000000, function(err)
+            {
+                assert.ifError(err);
+                done();
+            });
         });
 
         afterEach(function()
@@ -242,4 +385,61 @@ describe('OneLogin', function()
             nock.cleanAll();
         });
     });
+
+    describe('async', function()
+    {
+        it('methods resolve to promises with proper values', function(done)
+        {
+            nock('https://api.us.onelogin.com').post('/auth/oauth2/v2/token').reply(function(uri, requestBody) 
+            {
+                return [ 200, _tokenInfo ];
+            });
+
+            nock('https://api.us.onelogin.com').get('/api/1/users/10000000').reply(function(uri, requestBody) 
+            {
+                assert(this.req.headers['authorization']);
+                assert.match(this.req.headers['authorization'], new RegExp('bearer: ' + _tokenInfo.access_token));
+
+                var response = 
+                { 
+                    status: { error: false, code: 200, type: 'success', message: 'Success' },
+                    data: [ _users[0] ]
+                } 
+
+                return [ 200, response ];
+            });
+
+            nock('https://api.us.onelogin.com').get('/api/1/users/10000000/apps').reply(function(uri, requestBody) 
+            {
+                assert(this.req.headers['authorization']);
+                assert.match(this.req.headers['authorization'], new RegExp('bearer: ' + _tokenInfo.access_token));
+
+                var response = 
+                { 
+                    status: { error: false, code: 200, type: 'success', message: 'Success' },
+                    data: _apps
+                } 
+
+                return [ 200, response ];
+            });
+
+            var oneLogin = new OneLogin(clientId, clientSecret, "us").async();
+            oneLogin.getUser(10000000).then(user => 
+            {
+                assert.deepEqual(user, _users[0]);
+                return oneLogin.getUserApps(user.id)
+            })
+            .then(apps => 
+            {
+                assert.deepEqual(apps, _apps);
+                nock.cleanAll();
+                done();
+            })
+            .catch(function(err)
+            {
+                assert.ifError(err);
+            });
+        });
+    });
+
 });
