d854743822afa0c789b22b4584f9a431d5ada4e5
Dominick Caponi <d.caponi1@gmail.com>
2020-11-08 17:08:25 -0800

    updates run example

--- lib/run.ts
+++ lib/run.ts
@@ -6,8 +6,8 @@ import { OneLoginAppsRepository } from './onelogin/apps/apps'
 let region: Region = 'us'
 let timeout = 3000
 let baseURL = 'api.us.onelogin.com'
-let clientID = "8f05a9be54e9170d764c1172418038a87e26f97c61a09e69a7c6d866c75be746"
-let clientSecret = "beaf4a68baa183ef8c9036a296e0e29145c84364c98a551b5a1504ad44885cc9"
+let clientID = ""
+let clientSecret = ""
 
 let onelogin = new OneLoginHTTPClient(
   {clientID, clientSecret, region, timeout},
