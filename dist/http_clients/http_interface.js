f1d52f945787539898ba2b1928b7b05d23418bee
Dominick Caponi <d.caponi1@gmail.com>
2021-08-23 10:27:32 -0700

    rebuild

--- dist/http_clients/http_interface.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
-  HTTPClientAdapter
-  @describe Methods that must be implemented on classes that can be used as HTTP clients
-*/
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=http_interface.js.map
